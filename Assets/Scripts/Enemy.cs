2dd7e87b72148acfc492873bd8f8bd83721783d7
WYJustin1213 <wyjustin1213@outlook.com>
2025-12-27 14:35:59 -0600

    Death effect
    for enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,18 +5,46 @@ public class Enemy : MonoBehaviour
     public Animator animator;
     public Health health;
 
+    [Header("Death FX")]
+    [SerializeField] private GameObject[] deathParts;
+    [SerializeField] private float spawnForce;
+    [SerializeField] private float torque;
+    [SerializeField] private float lifeTime;
+
     private void OnEnable()
     {
         health.OnDamaged += HandleDamage;
+        health.OnDeath += HandleDeath;
     }
 
     private void OnDisable()
     {
         health.OnDamaged -= HandleDamage;
+        health.OnDeath -= HandleDeath;
     }
 
     void HandleDamage()
     {
         animator.SetTrigger("isHit");
+    }
+
+    void HandleDeath()
+    {
+        foreach (GameObject prefab in deathParts)
+        {
+            Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0.5f, 1)).normalized;
+            GameObject part = Instantiate(prefab, transform.position, rotation);
+
+            Rigidbody2D rb = part.GetComponent<Rigidbody2D>();
+
+            Vector2 randomDirection = new Vector2(Random.Range(-1, 1), Random.Range(0.5f, 1));
+
+            rb.linearVelocity = randomDirection * spawnForce;
+            rb.AddTorque(Random.Range(-torque, torque), ForceMode2D.Impulse);
+
+            Destroy(part, lifeTime);
+        }
+
+        Destroy(gameObject);
     }
 }
